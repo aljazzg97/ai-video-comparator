207247998f9baaf165c74f3d98f6f48cc8dc85a6
Aljaz g <aljaz.zg97@gmail.com>
2026-04-19 20:36:54 +0200

    Remove invalid smoothTouch property from Lenis config

--- video-comparison-frontend/app/layout.tsx
+++ video-comparison-frontend/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
       duration: 1.2,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
     });
 
     function raf(time: number) {
